07907eb2b5219fe4e99636e30a3ca596cce28c8d
deborahOkasaki <43588476+deborahOkasaki@users.noreply.github.com>
2019-04-12 10:13:43 -0300

    Add files via upload

---
+++ Andromeda-new branch/validacao.js
@@ -0,0 +1,44 @@
+function validarEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+  
+  function validar() {
+    var $result = $("#result");
+    var email = $("#email").val();
+    $result.text("");
+  
+    if (validarEmail(email)) {
+      $result.text();
+      $result.css("color", "green");
+    } else {
+      $result.text("Usuário ou senha inválidos! Tente novamente.");
+      $result.css("color", "#F4F192");
+    }
+    return false;
+  }
+
+  function validarSenha(senha) {
+    var re = /^[A-Za-z0-9]{5,}$/;
+    return re.test(senha);
+  }
+  
+  function validar() {
+    var $result = $("#result");
+    var senha = $("#senha").val();
+    $result.text("");
+  
+    if (validarSenha(senha)) {
+      $result.text();
+      $result.css("color", "green");
+    } else {
+      $result.text("Usuário ou senha inválidos! Tente novamente.");
+      $result.css("color", "#F4F192");
+    }
+    return false;
+  }
+
+  
+  $("#validar").bind("click", validar);
+
+  
